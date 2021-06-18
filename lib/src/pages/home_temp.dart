04815c9f410a597d8a0e41ce7bbdb7e333c60196
Kestler Barrios <luiseduardo3213@gmail.com>
2021-06-18 12:04:27 -0600

    Añadiendo ListTile

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Componentes App'),
       ),
-      body: ListView(
-        children: [
-          ListTile(
-            title: Text('Title'),
-          )
-        ],
-      ),
+      body: ListView(children: _crearItems()),
     );
+  }
+
+  List<Widget> _crearItems() {
+    List<Widget> lista = List<Widget>();
+
+    for (var item in opciones) {
+      final tempWidget = ListTile(
+        title: Text(item),
+      );
+
+      lista.add(tempWidget);
+    }
+
+    return lista;
   }
 }
