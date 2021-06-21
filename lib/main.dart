bbc6c1d3fb3c0655311e93973e6cdd11809911c8
Kestler Barrios <luiseduardo3213@gmail.com>
2021-06-21 16:41:42 -0600

    Utilizando rutas dinamicas

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:componentes/src/pages/alert_page.dart';
-import 'package:componentes/src/pages/avatar_page.dart';
+import 'package:componentes/src/routes/routes.dart';
 import 'package:flutter/material.dart';
-import 'package:componentes/src/pages/home_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,12 +10,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Componente App',
-      home: HomePage(),
       initialRoute: '/',
-      routes: {
-        '/': (BuildContext context) => HomePage(),
-        'alert': (BuildContext context) => AlertPage(),
-        'avatar': (BuildContext context) => AvatarPage(),
+      routes: getApplicationRoutes(),
+      onGenerateRoute: (settings) {
+        print('Ruta llamada ${settings.name}');
+        return MaterialPageRoute(builder: (context)=>AlertPage());
       },
     );
   }
