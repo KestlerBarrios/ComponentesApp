4ebb06fc71cafc6e06f5221fd462e1ff363211ab
Kestler Barrios <luiseduardo3213@gmail.com>
2021-06-22 13:05:48 -0600

    Pagina de Avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
 
 class AvatarPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text('Avatar Page'),
+        actions: [
+          Container(
+            padding: EdgeInsets.all(10),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://cnnespanol.cnn.com/wp-content/uploads/2015/08/sundar-pichai-google.jpg?quality=100&strip=info'),
+              radius: 25,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10),
+            child: CircleAvatar(
+              child: Text('SP'),
+              backgroundColor: Colors.brown,
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          placeholder: AssetImage('assets/images/loading1.gif'),
+          image: NetworkImage(
+              'https://s.france24.com/media/display/6aaac352-163c-11ea-9c6b-005056bff430/w:1280/p:16x9/ALPHABET-GOOGLE.webp'),
+        ),
       ),
     );
   }
