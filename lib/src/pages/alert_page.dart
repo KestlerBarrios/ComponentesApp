295dfb6913b42c70565c64fa38baace9d18ef7d9
Kestler Barrios <luiseduardo3213@gmail.com>
2021-06-22 12:46:25 -0600

    Widget Alert

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -8,11 +8,48 @@ class AlertPage extends StatelessWidget {
         centerTitle: true,
         title: Text('Alert Page'),
       ),
-      floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.exit_to_app),
-          onPressed: () {
-            Navigator.pop(context);
-          }),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Mostrar Alerta'),
+          style: ElevatedButton.styleFrom(
+            primary: Colors.blue,
+            shape: StadiumBorder(),
+          ),
+          onPressed: () => _mostrarAlerta(context),
+        ),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //     child: Icon(Icons.exit_to_app),
+      //     onPressed: () {
+      //       Navigator.pop(context);
+      //     }),
     );
+  }
+
+  void _mostrarAlerta(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            title: Text('Titulo'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('Este es el contenido de la caja alerta'),
+                FlutterLogo(
+                  size: 100,
+                )
+              ],
+            ),
+            actions: [
+              FlatButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text('Cancelar')),
+              FlatButton(onPressed: () {}, child: Text('Aceptar')),
+            ],
+          );
+        });
   }
 }
