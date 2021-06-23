48f846c18f9d13a3a70cb7fe3e1ff479d54f14c7
Kestler Barrios <luiseduardo3213@gmail.com>
2021-06-23 16:34:38 -0600

    Listview Builder

---
+++ lib/src/pages/listview_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ListaPage extends StatefulWidget {
+  @override
+  _ListaPageState createState() => _ListaPageState();
+}
+
+class _ListaPageState extends State<ListaPage> {
+  List<int> _listaNumeros = [1, 2, 3, 4, 5];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Listas'),
+      ),
+      body: _crearLista(),
+    );
+  }
+
+  Widget _crearLista() {
+    return ListView.builder(
+        itemCount: _listaNumeros.length,
+        itemBuilder: (BuildContext context, int index) {
+          final imagen = _listaNumeros[index];
+          return FadeInImage(
+              fadeInDuration: Duration(milliseconds: 200),
+              placeholder: AssetImage('assets/images/loading1.gif'),
+              image: NetworkImage('https://picsum.photos/id/$imagen/500/300'));
+        });
+  }
+}
